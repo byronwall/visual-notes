f42da32b96f9bc07a5c47369dceb9fd05aeceea5
Byron Wall <byron@byroni.us>
2025-11-01 23:07:02 -0400

    Add solid dev tools

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "@solidjs/start/config";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   ssr: true,
   middleware: "./src/middleware.ts",
   vite: {
+    plugins: [
+      devtools({
+        /* features options - all disabled by default */
+        autoname: true, // e.g. enable autoname
+        locator: {
+          targetIDE: "cursor",
+          componentLocation: true,
+          jsxLocation: true,
+        },
+      }),
+    ],
     ssr: {
       external: ["@prisma/client"],
     },
--- app/src/entry-client.tsx
+++ app/src/entry-client.tsx
@@ -1,3 +1,11 @@
 import { mount, StartClient } from "@solidjs/start/client";
+import "solid-devtools";
+
+import { attachDevtoolsOverlay } from "@solid-devtools/overlay";
+
+attachDevtoolsOverlay({
+  defaultOpen: false,
+  noPadding: true,
+});
 
 mount(() => <StartClient />, document.getElementById("app")!);
